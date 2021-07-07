6efa614466a5c57b01fef3fe0a52a13c936f7f78
Toan <tin4101999@gmail.com>
2021-07-07 10:03:51 +0700

    Register-Login-Logout

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,14 +9,9 @@ class Task extends Model
 {
     use HasFactory;
     protected $fillable=[
-<<<<<<< HEAD
         'name',
     ];
     
-=======
-        'name'
-    ];
->>>>>>> 722619b627dc79a9979dd741ddabfe720d9649dc
     public function user()
     {
         return $this->belongsTo(User::class);
